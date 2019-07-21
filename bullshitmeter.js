3ba71ec8c4ca3b20fd6ccd6df1a6d4ececb768d6
Simon Verhoek <simon@kaartencarrousel.nl>
2019-07-21 13:33:21 +0200

    comment boxes now show and hide according to text in textarea

--- bullshitmeter.js
+++ bullshitmeter.js
@@ -1,26 +1,46 @@
-$(document).ready(function () {
-  console.log('test');
+$(function () {
+  const jqxhr = $.getJSON('glossary.json');
 
-  const text_input = $('#text_input');
-  const comment_section = $('#comment_section');
-  const comment_box_sample = $('.comment_box.sample');
-  let comments = [];
+  $(document).ready(function () {
+    const text_input = $('#text_input');
+    const comment_section = $('#comment_section');
+    const comment_box_sample = $('.comment_box.sample');
+    let comments = {};
+    let glossary;
 
-  $(text_input).on('input', function (e) {
-    if (e.target.value === 'puppy') {
-      let comment_box = comment_box_sample.clone().removeClass('sample');
-
-      $(comment_box).children('p').first().text(e.target.value);
-
-      comments.push(comment_box);
-    } else {
-      comments.length = 0;
+    function updateCommentSection() {
+      comment_section.empty();
+      $.each(comments, function (term, comment_box) {
+        comment_box = comment_box[0];
+        comment_section.append(comment_box)
+      })
     }
 
-    for (let i = 0; i < comments.length; i++) {
-      comment_section.append(comments[i])
-    }
-  })
+    jqxhr.done(function (data) {
+      glossary = data.words;
 
+      const glossary_terms = Object.keys(glossary);
 
+      $(text_input).on('input', function (e) {
+        const text = e.target.value;
+
+        for (let i = 0; i < glossary_terms.length; i++) {
+          const term = glossary_terms[i];
+
+          if (text.indexOf(term.toLowerCase()) < 0) {
+            delete comments[term]
+          } else if (!comments[term]) {
+            let comment_box = comment_box_sample.clone().removeClass('sample');
+
+            $(comment_box).children('p').first().text(term + ': ' + glossary[term]);
+
+            comments[term] = comment_box;
+          }
+        }
+
+        updateCommentSection();
+      })
+
+    });
+  });
 });
